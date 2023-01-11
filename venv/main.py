6b93b191c5cc5710a0a57390107067ec9a8a6a09
edm0322 <edm0322@gmail.com>
2023-01-11 09:56:00 +0900

    쿼리 매개변수 학습 완료

--- venv/main.py
+++ venv/main.py
@@ -1,6 +1,8 @@
 from enum import Enum
 
-from typing import Union
+#3.10 버전 이상 Union을 안써도 변경 가능 
+#https://peps.python.org/pep-0604/
+#from typing import Union
 
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -17,7 +19,8 @@ fake_item_db = [
 class Item(BaseModel):
     name: str
     price: float
-    is_offer: Union[bool, None] = None #Default 지정자
+    is_offer: bool | None = None #Default 지정자
+    #is_offer: Union[bool, None] = None
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
@@ -48,10 +51,10 @@ async def root():
 async def read_item(skip: int = 0, limit: int = 10):
     return fake_item_db[skip : skip + limit]
 '''
-
+'''
 ### Query Parameter Type Conversion
 @app.get("/items/{item_id}")
-async def read_item(item_id: str, q: Union[str, None] = None, short: bool = False):
+async def read_item(item_id: str, q: str | None = None, short: bool = False): 
     item = {"item_id": item_id}
     if q:
         item.update({"q": q})
@@ -60,7 +63,38 @@ async def read_item(item_id: str, q: Union[str, None] = None, short: bool = Fals
             {"description": "This is an amazing item that has a long description"}
         )
     return item
+'''
+#여러 경로 / 쿼리 매개 변수
+'''
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int,
+    item_id: str,
+    q: str | None = None,
+    short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
 
+    if q:
+        item.update({"q": q })
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+'''
+
+'''
+#필수 쿼리 매개변수
+
+@app.get("/items/{item_id}")
+async def read_user_item(
+    item_id: str,
+    needy: str
+):
+    item = {"item_id": item_id, "needy": needy}
+    return item
+'''
 '''
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
@@ -69,6 +103,25 @@ def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id} #q가 None일 경우
 '''
 
+#종합쿼리
+@app.get("/items/{items_id}")
+async def read_user_item(
+    item_id: str,
+    needy: str,
+    skip: int = 0,
+    limit: int | None = None
+):
+    item = {
+        "item_id": item_id,
+        "needy": needy,
+        "skip": skip,
+        "limit": limit
+    }
+    return item
+
+#PUT Method 사용
+'''
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_price": item.price, "item_id": item_id}
+'''
