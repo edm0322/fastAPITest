f57791e02d6d48d6807c723d90619861969e808d
edm0322 <edm0322@gmail.com>
2023-01-11 11:57:02 +0900

    Declear more metadata 부터 진행

--- venv/main.py
+++ venv/main.py
@@ -64,3 +64,70 @@ async def read_items(q: str = Query(defalut="fixedquery", min_length=3)):
     return results
 '''
 
+'''
+#Using Ellipsis
+@app.get("/items/")
+async def read_items(q: str = Query(default=..., min_length=3)):
+    results = {
+        "items":[
+            {"items_id", "foo"},
+            {"items_id", "bar"}
+        ]
+    }
+    if q:
+        results.update({"q": q})
+    return results
+'''
+
+'''
+# Using Ellipsis with None
+@app.get("/items/")
+async def read_items(q: str | None = Query(default=..., min_length=3)):
+    results = {
+        "items": [
+            {"item_id":"foo"},
+            {"item_id":"bar"}
+        ]
+    }
+    if q:
+        results.update({"q":q})
+    return results
+'''
+
+'''
+# Using PYdantic Required
+from pydantic import Required
+
+@app.get("/items/")
+async def read_items(q: str = Query(default=Required, min_length=3)):
+    results = {"items": [
+            {"item_id": "foo"},
+            {"item_id": "bar"}
+        ]
+    }
+    if q:
+        results.update({"q" : q})
+    return results
+'''
+
+'''
+# Query List / Multiple Value
+@app.get("/items/")
+async def read_items(q: list[str] | None = Query(default=None)):
+    query_items = {"q": q}
+    return query_items
+'''
+
+
+# Query List / Multiple Value Defaults
+'''
+@app.get("/items/")
+#async def read_items(q: list[str] = Query(default=['foo', 'bar'])):
+## List Default
+async def read_item(q: list = Query(default=[])):
+    query_items = {"q":q}
+    return query_items
+
+'''
+
+
